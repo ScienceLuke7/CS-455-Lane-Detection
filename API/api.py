5cdf554539f8ac28c27909482f005bb111823c5c
Christopher Etinas <luke100700@gmail.com>
2022-04-20 20:25:27 -0400

    api routes

--- API/api.py
+++ API/api.py
@@ -1,5 +1,7 @@
 import json
 import pickle
+import cv2
+import glob
 from flask import Flask, jsonify, request, send_file
 # import neural_network.model_evaluator
 
@@ -11,15 +13,23 @@ def home():
 
 @app.route("/postRawImage", methods=["POST"])
 def getProcessedImage():
-    image = pickle.loads(request.data)
-    print(image)
-    return
+    image = request.data
+    # print(image)
+    return send_file('../_datasets/train_set/clips/0313-1/120/10.jpg', mimetype='image/jpeg')
 
 @app.route("/postRawVideo", methods=["POST"])
 def getProcessedVideo():
     video = request.data
-    print(video)
-    return
+    # print(video)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('./output_video.mp4', fourcc, 20, (1280,720), isColor=True)
+    
+    for filename in glob.glob('./_datasets/train_set/clips/0313-1/120/*.jpg'):
+        img = cv2.imread(filename)
+        out.write(img)
+
+    out.release()
+    return send_file('./output_video.mp4', mimetype='video/mp4')
 
 
 
